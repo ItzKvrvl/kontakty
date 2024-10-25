808f605c2622b48151ae50976b92849326ace8b9
ItzKvrvl <uT_4005@zsp3zamosc.pl>
2024-10-25 23:33:31 +0200

    kontakty pierwsze

---
+++ src/kontakty.js
@@ -0,0 +1,42 @@
+
+console.log();
+console.log('Zadanie. 1');
+console.log('----------------');
+console.log("W tym zadaniu nic nie mialo sie wyswietlic w konsoli C:");
+console.log();
+const contacts=[
+    {
+        name:"Maxwell Wright",
+        phone:"(0191)719 6495",
+        email:"Curab.nunc@nomm.com",
+    },
+    {
+        name:"Raja Villarreal",
+        phone:"0866 398 2895",
+        email:"posuere.vulputate@sed.com",
+    },
+    {
+        name:"Helen Richards",
+        phone:"0800 1111",
+        email:"libero@convallis.edu",
+    },
+];//-----------------------------
+console.log();
+console.log("Zadanie. 2");
+console.log("----------------");
+console.log();
+
+contacts.push({
+    name:"Maisie Haley",
+    phone:"0913 531 3030",
+    email:"risus.Quisque@urna.ca",
+});
+
+console.log(contacts.length);
+console.log("--Dane 1 osoby--")
+console.log();
+console.log();
+console.log(contacts[0]);
+console.log("--Dane ostatniej osoby--")
+console.log(contacts[3]);
+
